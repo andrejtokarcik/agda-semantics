42bbaf4c9ed9d760861f655fd3f7edb75130a485
Andrej Tokarčík <andrejtokarcik@gmail.com>
2015-04-14 17:49:50 +0200

    more latex goodness

--- agda-parser/kompile-latex.sh
+++ agda-parser/kompile-latex.sh
@@ -3,4 +3,5 @@ set -e
 
 kompile --backend latex -d latex agda.k -v
 cp latex/agda.tex latex/agda.tex.orig
-./latex/postprocess.py latex/agda.tex
+echo Postprocessing...
+./latex/postprocess.py latex/agda.tex.orig >latex/agda.tex
