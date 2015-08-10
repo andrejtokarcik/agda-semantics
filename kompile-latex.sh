8dbc14ca9f9b5711b52d2cf44a2025e2a3e37073
Andrej Tokarčík <andrejtokarcik@gmail.com>
2015-08-10 21:44:53 +0200

    rm kompile-latex.sh

--- kompile-latex.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-LATEX_DIR=~/ltx/k/
-
-K_OPTS="-Xms100m -Xmx12000m -Xss50m" kompile --backend latex -d $LATEX_DIR agda.k -v
-cp $LATEX_DIR/agda.tex $LATEX_DIR/agda.tex.orig
-echo Postprocessing...
-$LATEX_DIR/postprocess.py $LATEX_DIR/agda.tex.orig >$LATEX_DIR/agda.tex
